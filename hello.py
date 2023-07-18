7f8e3dd37d8f52c90ca6be47d5745e7f853a2388
Artem Nastasev <a.nastas2000@yandex.ru>
2023-07-18 20:16:11 +0500

    18.07.23

---
+++ hello.py
@@ -0,0 +1,18 @@
+def discounted(price, discount, max_discount=50):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    max_discount = abs(float(max_discount))
+    if max_discount > 99:
+        raise ValueError("Максимальная скидка не может быть больше 99%")
+    if discount >= 100:
+        price_with_discount = price
+    else:
+        price_with_discount = price - price * discount / 100
+    return(price_with_discount) 
+    
+discounted(100, -50)
+print(discounted(-300, 5))
+discounted(231, 111,)
+
+
+          
